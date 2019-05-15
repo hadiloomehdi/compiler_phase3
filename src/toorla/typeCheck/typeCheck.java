f798e879bdd27bb5022252104182a55bccceaa5d
mahdi <hadiloomehdi@gmail.com>
2019-05-15 11:13:03 +0430

    initial type check file

--- src/toorla/typeCheck/typeCheck.java
+++ src/toorla/typeCheck/typeCheck.java
@@ -33,218 +33,286 @@ import java.util.ArrayList;
 
 public class typeCheck implements Visitor<Void> {
 
-        @Override
-        public Void visit(PrintLine printStat) {
-            return null;
+    @Override
+    public Void visit(PrintLine printLine) {
+        printLine.getArg().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(Assign assign) {
+        assign.getLvalue().accept(this);
+        assign.getRvalue().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(Block block) {
+        for (Statement s : block.body)
+            s.accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(Conditional conditional) {
+        conditional.getCondition().accept(this);
+        conditional.getThenStatement().accept(this);
+        conditional.getElseStatement().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(While whileStat) {
+        whileStat.expr.accept(this);
+        whileStat.body.accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(Return returnStat) {
+        returnStat.getReturnedExpr().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(Plus plusExpr) {
+        plusExpr.getLhs().accept(this);
+        plusExpr.getRhs().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(Minus minusExpr) {
+        minusExpr.getLhs().accept(this);
+        minusExpr.getRhs().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(Times timesExpr) {
+        timesExpr.getLhs().accept(this);
+        timesExpr.getRhs().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(Division divisionExpr) {
+        divisionExpr.getLhs().accept(this);
+        divisionExpr.getRhs().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(Modulo moduloExpr) {
+        moduloExpr.getLhs().accept(this);
+        moduloExpr.getRhs().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(Equals equalsExpr) {
+        equalsExpr.getLhs().accept(this);
+        equalsExpr.getRhs().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(GreaterThan gtExpr) {
+        gtExpr.getLhs().accept(this);
+        gtExpr.getRhs().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(LessThan ltExpr) {
+        ltExpr.getLhs().accept(this);
+        ltExpr.getRhs().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(And andExpr) {
+        andExpr.getLhs().accept(this);
+        andExpr.getRhs().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(Or orExpr) {
+        orExpr.getLhs().accept(this);
+        orExpr.getRhs().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(Neg negExpr) {
+        negExpr.getExpr().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(Not notExpr) {
+        notExpr.getExpr().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(MethodCall methodCall) {
+        methodCall.getInstance().accept(this);
+        methodCall.getMethodName().accept(this);
+        for (Expression arg : methodCall.getArgs()) {
+            arg.accept(this);
         }
+        return null;
+    }
 
-        @Override
-        public Void visit(Assign assignStat) {
-            return null;
+    @Override
+    public Void visit(Identifier identifier) {
+        return null;
+    }
+
+    @Override
+    public Void visit(Self self) {
+        return null;
+    }
+
+    @Override
+    public Void visit(Break breakStat) {
+        return null;
+    }
+
+    @Override
+    public Void visit(Continue continueStat) {
+        return null;
+    }
+
+    @Override
+    public Void visit(Skip skip) {
+        return null;
+    }
+
+    @Override
+    public Void visit(IntValue intValue) {
+        return null;
+    }
+
+    @Override
+    public Void visit(NewArray newArray) {
+        newArray.getLength().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(BoolValue booleanValue) {
+        return null;
+    }
+
+    @Override
+    public Void visit(StringValue stringValue) {
+        return null;
+    }
+
+    @Override
+    public Void visit(NewClassInstance newClassInstance) {
+        newClassInstance.getClassName().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(FieldCall fieldCall) {
+        fieldCall.getInstance().accept(this);
+        fieldCall.getField().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(ArrayCall arrayCall) {
+        arrayCall.getInstance().accept(this);
+        arrayCall.getIndex().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(NotEquals notEquals) {
+        notEquals.getLhs().accept(this);
+        notEquals.getRhs().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(LocalVarDef localVarDef) {
+        localVarDef.getLocalVarName().accept(this);
+        localVarDef.getInitialValue().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(IncStatement incStatement) {
+        incStatement.getOperand().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(DecStatement decStatement) {
+        decStatement.getOperand().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(ClassDeclaration classDeclaration) {
+        printClassBody(classDeclaration);
+        return null;
+    }
+
+    private void printClassBody(ClassDeclaration classDeclaration) {
+        classDeclaration.getName().accept(this);
+        if (classDeclaration.getParentName().getName() != null) {
+            classDeclaration.getParentName().accept(this);
         }
+        for (ClassMemberDeclaration md : classDeclaration.getClassMembers())
+            md.accept(this);
+    }
 
-        @Override
-        public Void visit(Block block) {
-            return null;
+    @Override
+    public Void visit(EntryClassDeclaration entryClassDeclaration) {
+        printClassBody(entryClassDeclaration);
+        return null;
+    }
+
+    @Override
+    public Void visit(FieldDeclaration fieldDeclaration) {
+        fieldDeclaration.getIdentifier().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(ParameterDeclaration parameterDeclaration) {
+        parameterDeclaration.getIdentifier().accept(this);
+        return null;
+    }
+
+    @Override
+    public Void visit(MethodDeclaration methodDeclaration) {
+        methodDeclaration.getName().accept(this);
+        for (ParameterDeclaration pd : methodDeclaration.getArgs()) {
+            pd.accept(this);
         }
+        for (Statement stmt : methodDeclaration.getBody())
+            stmt.accept(this);
+        return null;
+    }
 
-        @Override
-        public Void visit(Conditional conditional) {
-            return null;
-        }
+    @Override
+    public Void visit(LocalVarsDefinitions localVarsDefinitions) {
+        for (LocalVarDef lvd : localVarsDefinitions.getVarDefinitions())
+            lvd.accept(this);
+        return null;
+    }
 
-        @Override
-        public Void visit(While whileStat) {
-            return null;
-        }
-
-        @Override
-        public Void visit(Return returnStat) {
-            return null;
-        }
-
-        @Override
-        public Void visit(Break breakStat) {
-
-            return null;
-        }
-
-        @Override
-        public Void visit(Continue continueStat) {
-            return null;
-        }
-
-        @Override
-        public Void visit(Skip skip) {
-            return null;
-        }
-
-        @Override
-        public Void visit(LocalVarDef localVarDef) {
-
-            return null;
-        }
-
-        @Override
-        public Void visit(IncStatement incStatement) {
-            return null;
-        }
-
-        @Override
-        public Void visit(DecStatement decStatement) {
-            return null;
-        }
-
-        @Override
-        public Void visit(Plus plusExpr) {
-            return null;
-        }
-
-        @Override
-        public Void visit(Minus minusExpr) {
-            return null;
-        }
-
-        @Override
-        public Void visit(Times timesExpr) {
-            return null;
-        }
-
-        @Override
-        public Void visit(Division divExpr) {
-            return null;
-        }
-
-        @Override
-        public Void visit(Modulo moduloExpr) {
-            return null;
-        }
-
-        @Override
-        public Void visit(Equals equalsExpr) {
-            return null;
-        }
-
-        @Override
-        public Void visit(GreaterThan gtExpr) {
-            return null;
-        }
-
-        @Override
-        public Void visit(LessThan lessThanExpr) {
-            return null;
-        }
-
-        @Override
-        public Void visit(And andExpr) {
-            return null;
-        }
-
-        @Override
-        public Void visit(Or orExpr) {
-            return null;
-        }
-
-        @Override
-        public Void visit(Neg negExpr) {
-            return null;
-        }
-
-        @Override
-        public Void visit(Not notExpr) {
-            return null;
-        }
-
-        @Override
-        public Void visit(MethodCall methodCall) {
-            return null;
-        }
-
-        @Override
-        public Void visit(Identifier identifier) {
-            return null;
-        }
-
-        @Override
-        public Void visit(Self self) {
-            return null;
-        }
-
-        @Override
-        public Void visit(IntValue intValue) {
-            return null;
-        }
-
-        @Override
-        public Void visit(NewArray newArray) {
-            return null;
-        }
-
-        @Override
-        public Void visit(BoolValue booleanValue) {
-            return null;
-        }
-
-        @Override
-        public Void visit(StringValue stringValue) {
-            return null;
-        }
-
-        @Override
-        public Void visit(NewClassInstance newClassInstance) {
-            return null;
-        }
-
-        @Override
-        public Void visit(FieldCall fieldCall) {
-            return null;
-        }
-
-        @Override
-        public Void visit(ArrayCall arrayCall) {
-            return null;
-        }
-
-        @Override
-        public Void visit(NotEquals notEquals) {
-            return null;
-        }
-
-        @Override
-        public Void visit(ClassDeclaration classDeclaration) {
-            return null;
-        }
-
-        @Override
-        public Void visit(EntryClassDeclaration entryClassDeclaration) {
-            return null;
-        }
-
-        @Override
-        public Void visit(FieldDeclaration fieldDeclaration) {
-
-            return null;
-        }
-
-        @Override
-        public Void visit(ParameterDeclaration parameterDeclaration) {
-
-            return null;
-        }
-
-        @Override
-        public Void visit(MethodDeclaration methodDeclaration) {
-
-            return null;
-        }
-
-        @Override
-        public Void visit(LocalVarsDefinitions localVarsDefinitions) {
-            return null;
-        }
-
-        @Override
-        public Void visit(Program program) {
-            return null;
-        }
+    @Override
+    public Void visit(Program program) {
+        for (ClassDeclaration cd : program.getClasses())
+            cd.accept(this);
+        return null;
+    }
 }
